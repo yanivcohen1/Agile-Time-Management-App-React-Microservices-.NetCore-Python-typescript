b41b70c91f36acb27d0b015abc78f71db6c5c9ac
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2025-12-18 11:32:17 +0200

    add cypress workin dir to vsc config

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "cypress";
 import { createRequire } from "module";
+import { resolve } from "path";
 const require = createRequire(import.meta.url);
 
 export default defineConfig({
-   e2e: {
+  e2e: {
     baseUrl: 'http://localhost:5173',
     supportFile: false,
     reporter: process.env.CYPRESS_REPORTER || 'spec',
@@ -12,6 +13,9 @@ export default defineConfig({
       overwrite: true,
       html: true,
       json: false
+    },
+    env: {
+      NODE_PATH: resolve('./node_modules')
     },
     setupNodeEvents(on) {
       if (process.env.CYPRESS_REPORTER === 'cypress-mochawesome-reporter') {
